7cb96593a4ea947140976bb3702a4ec78a27c32e
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-03-06 16:43:50 +0100

    Create state varibales

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react"
 import Header from "./components/Header"
 import InputFields from "./components/InputFields"
 function App() {
+const [initialInvestment, setInitialInvestment] = useState(0)
+const [annualInvestment, setAnnualInvestment] = useState(0)
+const [expectedReturn, setExpectedReturn] = useState(0)
+const [duration, setDuration] = useState(0)  
   return (
     <>
       <Header/>
-      <InputFields/>
+      <InputFields initialInvestment={initialInvestment} annualInvestment={annualInvestment} expectedReturn={expectedReturn} duration={duration}/>
     </>
   )
 }
--- src/components/InputFields.jsx
+++ src/components/InputFields.jsx
@@ -1,22 +1,22 @@
-const InputFields = () => {
+const InputFields = ({initialInvestment, annualInvestment, expectedReturn, duration}) => {
   return (
     <section id="user-input">
       <div className="input-group">
         <p>
         <label htmlFor="">Initial Investment</label>
-        <input type="number" required/>
+        <input type="number" value={initialInvestment} required/>
         </p>
         <p>
         <label htmlFor="">Annual Investment</label>
-        <input type="number" required/>
+        <input type="number" value={annualInvestment} required/>
         </p>
         <p>
         <label htmlFor="">Expected Return</label>
-        <input type="number" required/>
+        <input type="number" value={expectedReturn} required/>
         </p>
         <p>
         <label htmlFor="">Duration</label>
-        <input type="number" required/>
+        <input type="number" value={duration} required/>
         </p>
       </div>
     </section>
